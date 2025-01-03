f955d289c0364d1f4f4295768d6765ccee35beda
stefbp066 <stefan.pang99@gmail.com>
2025-01-04 00:46:50 +0100

    Revert "add dockerfile, gitignore extended, locally tested"
    This reverts commit a02603c6eb937fbf7ac410b2cf498645abceb5ed.

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def index():
     return render_template('index.html', answer=answer)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
+    app.run(debug=True, threaded=True)
